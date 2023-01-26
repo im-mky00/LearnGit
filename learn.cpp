58c634efa1e98e563c4170784fa0c53a0efe556d
Manish Kumar Yadav <kumaryadavmanish00@gmail.com>
2023-01-26 22:40:19 +0530

    initial commit

---
+++ learn.cpp
@@ -0,0 +1,20 @@
+#include"bits/stdc++.h"
+using namespace std;
+using ll = long long;
+#define vi vector<int>
+#define pii pair<int,int>
+void solve()
+{
+  cout<<"Learn Git";
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+  freopen("er.txt", "w", stderr);
+#endif
+  // long long int t; cin >> t; while (t--)
+  {solve();}
+  return 0;
+
+}
