ab1236a773dd74f233e280097c73e39aaa4372f4
Manish Kumar Yadav <kumaryadavmanish00@gmail.com>
2023-01-26 23:38:13 +0530

    Multiplication part added

---
+++ multiply.cpp
@@ -0,0 +1,23 @@
+#include"bits/stdc++.h"
+using namespace std;
+using ll = long long;
+#define vi vector<int>
+#define pii pair<int,int>
+void solve()
+{
+  int a,b;
+  cin>>a>>b;
+  int product=a*b;
+  cout<<product;
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+  freopen("er.txt", "w", stderr);
+#endif
+  // long long int t; cin >> t; while (t--)
+  {solve();}
+  return 0;
+
+}
